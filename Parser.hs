54983b91ce107cfdff968127c4fd49e6cd34acc1
Connor Williams <connor@janrain.com>
2013-03-17 19:59:05 -0700

    add parseExpr

--- Parser.hs
+++ Parser.hs
@@ -1,5 +1,6 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Applicative ((<$>))
 
 main = do
     args <- getArgs
@@ -9,12 +10,35 @@ main = do
 symbol = oneOf "!#%&|*+-/?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many $ noneOf "\""
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many $ letter <|> digit <|> symbol
+               let atom = first : rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
 
 data LispVal = Atom String
              | List [LispVal]
