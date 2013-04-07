37fb69d225251d31ee1b2ce3fe03240e30abbf38
Connor Williams <connor@janrain.com>
2013-04-06 17:14:21 -0700

    add primitive eq? and eqv? functions

--- Parser.hs
+++ Parser.hs
@@ -174,6 +174,8 @@ primitives = [ ("+", numericBinop (+))
              , ("car", car)
              , ("cdr", cdr)
              , ("cons", cons)
+             , ("eq?", eqv)
+             , ("eqv?", eqv)
              ]
 
 boolBinop unpacker op args = if length args /= 2
@@ -235,3 +237,18 @@ cons [x, List xs] = return $ List $ x : xs
 cons [x, DottedList xs y] = return $ DottedList (x:xs) y
 cons [x, y] = return $ DottedList [x] y
 cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] =
+    eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2)
+                              && (all eqvPair $ zip arg1 arg2)
+    where eqvPair (x1, x2) = case eqv [x1, x2] of
+                                 Left err -> False
+                                 Right (Bool val) -> val
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
